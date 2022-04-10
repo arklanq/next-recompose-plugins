3cc41fd7f0bac28b8cd73f42e6894675eae777e1
Arkadiusz Nurzyński <nowyarek@gmail.com>
2022-04-10 15:08:41 +0200

    chore: setup source code related tools

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+      },
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+      ],
+    },
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+  bracketSpacing: false
+};
