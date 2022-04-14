4a11bc983e0f481b3977536ad2863b60c6a96b57
Arkadiusz Nurzyński <nowyarek@gmail.com>
2022-04-14 22:06:30 +0200

    feat: migrate codebase to monorepo

--- lib/src/ConfigBuilder.ts
+++ lib/src/ConfigBuilder.ts
@@ -5,6 +5,8 @@ import {
 } from './NextConfigDeclaration';
 import {NextConfig} from 'next';
 import {PluginApplyAction} from './NextPlugin';
+import isPromise from 'is-promise';
+import {isAsyncConfigFactorySupported} from './isAsyncConfigFactorySupported';
 
 export class ConfigBuilder {
   private readonly configFactory: NextConfigFactory;
@@ -32,20 +34,25 @@ export class ConfigBuilder {
     }
   }
 
-  static defineConfig(config: NextConfig): ConfigBuilder;
-  static defineConfig(configFactory: NextConfigFactory): ConfigBuilder;
   static defineConfig(configDeclaration: NextConfigDeclaration): ConfigBuilder {
     return new ConfigBuilder(configDeclaration);
   }
 
   applyPlugin(applyAction: PluginApplyAction, pluginName?: string): ConfigBuilder {
+    if(isAsyncConfigFactorySupported())
+      return this.applyPluginAsync(applyAction, pluginName);
+    else
+      return this.applyPluginSync(applyAction, pluginName);
+  }
+
+  private applyPluginAsync(applyAction: PluginApplyAction, pluginName?: string): ConfigBuilder {
     if (typeof applyAction !== 'function')
       throw new Error(`Invalid 1st argument (\`applyAction\`) passed to \`configurePlugin\` method. Expected function, instead got \`${typeof applyAction}\`.'`);
 
     const newConfigFactory: NextConfigFactory = async (phase: ConfigurationPhase, args: ConfigFactoryArguments) => {
       let newConfig: NextConfig;
       try {
-        newConfig = this.configFactory(phase, args);
+        newConfig = await this.configFactory(phase, args);
       } catch(e: unknown) {
         console.error('Function passed to `defineConfig` method threw na error during the configuration step.');
         throw e;
@@ -63,6 +70,52 @@ export class ConfigBuilder {
 
         throw e;
       }
+    };
+
+    newConfigFactory.pluginName = pluginName;
+
+    return new ConfigBuilder(newConfigFactory);
+  }
+
+  private applyPluginSync(applyAction: PluginApplyAction, pluginName?: string): ConfigBuilder {
+    if (typeof applyAction !== 'function')
+      throw new Error(`Invalid 1st argument (\`applyAction\`) passed to \`configurePlugin\` method. Expected function, instead got \`${typeof applyAction}\`.'`);
+
+    const newConfigFactory: NextConfigFactory = (phase: ConfigurationPhase, args: ConfigFactoryArguments): NextConfig => {
+      let newConfig: NextConfig;
+      try {
+        newConfig = this.configFactory(phase, args);
+      } catch(e: unknown) {
+        console.error('Function passed to `defineConfig` method threw na error during the configuration step.');
+        throw e;
+      }
+
+      if(isPromise(newConfig))
+        throw new Error('Function passed to `defineConfig` method returned Promise. This feature is supported only in Next.js 12.1+.');
+
+      try {
+        newConfig = applyAction(phase, args, newConfig);
+      } catch(e: unknown) {
+        if(pluginName)
+          console.error(`Function applying \`${pluginName}\` plugin threw an error during the configuration step.`);
+        else if(applyAction.name)
+          console.error(`Function \`${applyAction.name}\` passed to \`applyPlugin\` method threw an error during the configuration step.`);
+        else
+          console.error('One of the plugin application functions threw an error during the configuration step.');
+
+        throw e;
+      }
+
+      if(isPromise(newConfig)) {
+        if(pluginName)
+          console.error(`Function applying \`${pluginName}\` plugin returned Promise. This feature is supported only in Next.js 12.1+.`);
+        else if(applyAction.name)
+          console.error(`Function \`${applyAction.name}\` passed to \`applyPlugin\` method returned Promise. This feature is supported only in Next.js 12.1+.`);
+        else
+          console.error('One of the plugin application functions returned Promise. This feature is supported only in Next.js 12.1+.');
+      }
+
+      return newConfig;
     };
 
     newConfigFactory.pluginName = pluginName;
---
+++ lib/src/isAsyncConfigFactorySupported.ts
@@ -0,0 +1,11 @@
+import semver from 'semver';
+
+export function isAsyncConfigFactorySupported() {
+  const nextJsModulePath = require.resolve('next/package.json', {paths: [process.cwd()]});
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const nextJsPkg = require(nextJsModulePath) as {version: string};
+  const nextJsVersion: string = nextJsPkg.version;
+
+  return semver.gt(nextJsVersion, '12.1.0');
+}
