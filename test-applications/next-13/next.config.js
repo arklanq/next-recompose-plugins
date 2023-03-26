9b867b4186934f8ada9ca42c3847edd752456383
Arkadiusz Nurzyński <nowyarek@gmail.com>
2023-03-26 17:40:11 +0200

    feat(test-applications): create new test-applications `next-13`

---
+++ test-applications/next-13/next.config.js
@@ -0,0 +1,21 @@
+const {Config} = require('next-recompose-plugins');
+const {PHASE_PRODUCTION_BUILD} = require('next/constants');
+const withBundleAnalyzer = require('@next/bundle-analyzer');
+
+const config = new Config(async () => {
+  return {
+    reactStrictMode: true
+  };
+})
+  .applyPlugin((phase, args, config) => {
+    return withBundleAnalyzer({enabled: phase === PHASE_PRODUCTION_BUILD})(config);
+  }, 'bundle-analyzer')
+  .applyPlugin((phase, args, config) => {
+    config.env = {
+      foo: 'bar'
+    };
+    return config;
+  }, 'dummy-plugin')
+  .build();
+
+module.exports = config;
